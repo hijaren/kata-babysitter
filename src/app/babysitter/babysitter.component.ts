89327afc4df196d6db2aead24d53581d0c1d4d60
Jann <jannrene.ibay@gmail.com>
2021-04-06 20:47:17 -0500

    Created 'babysitter' component

---
+++ src/app/babysitter/babysitter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-babysitter',
+  templateUrl: './babysitter.component.html',
+  styleUrls: ['./babysitter.component.css']
+})
+export class BabysitterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
